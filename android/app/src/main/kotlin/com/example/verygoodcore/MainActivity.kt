4198e62cbc358df72c23a1fce112b6f6e1c918cc
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2024-02-06 16:53:12 +0400

    First commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.landing_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:landing_app/app/app.dart';
+import 'package:landing_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
